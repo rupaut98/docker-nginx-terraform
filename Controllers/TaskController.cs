ed07763e197e98a273ca695da3a40347cb76a31a
rupaut98 <rupakraut78@gmail.com>
2024-10-31 17:29:03 -0500

    Added models and controllers

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using docker_nginx_terraform.Models;
+using System.Collections.Generic;
+using System.Linq;
+using TaskModel = docker_nginx_terraform.Models.Task;
+
+namespace docker_nginx_terraform.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class TaskController : ControllerBase{
+
+        private static List<TaskModel> tasks = new List<TaskModel>();
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<TaskModel>> GetTasks(){
+            return Ok(tasks);
+        }
+
+        // GET: api/task/{id}
+        [HttpGet("{id}")]
+        public ActionResult<TaskModel> GetTaskById(int id){
+            TaskModel task = null;
+
+            foreach (var t in tasks){
+                if (t.Id == id){
+                    task = t;
+                    break;
+                }
+            }
+
+            if (task == null){
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        //POST: api/task
+        [HttpPost]
+        public ActionResult<TaskModel> CreateTask(TaskModel newTask){
+            
+            if (tasks.Count > 0){
+                int MaxId = tasks.Max(t => t.Id);
+                newTask.Id = MaxId + 1;
+            }
+            else{
+                newTask.Id = 1;
+            }
+            tasks.Add(newTask);
+            return CreatedAtAction(nameof(GetTaskById), new {id = newTask.Id}, newTask);
+        }
+    }
+}
