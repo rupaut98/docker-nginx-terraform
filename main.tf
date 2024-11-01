85ba81e973bb0dc4de9c549210f7c175f0811e7e
rupaut98 <rupakraut78@gmail.com>
2024-11-01 13:44:07 -0500

    Removed NAT to minimze incurred costs

--- main.tf
+++ main.tf
@@ -12,51 +12,24 @@ resource "aws_vpc" "main_vpc"{
     }
 }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main_vpc.id 
-    
-    tags = {
-        Name = "main_igw"
-    }
-    }
-
-resource "aws_subnet" "public_subnet"{
+resource "aws_subnet" "private_subnet_1"{
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-west-2a"
-    map_public_ip_on_launch = true
 
     tags = {
-        Name = "public_subnet"
+        Name = "private_subnet_1"
     }
 }
 
-resource "aws_subnet" "private_subnet"{
+resource "aws_subnet" "private_subnet_2"{
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = "10.0.2.0/24"
-    availability_zone = "us-west-2a"
+    availability_zone = "us-west-2b"
 
     tags = {
-        Name = "private_subnet"
+        Name = "private_subnet_2"
     }
-}
-
-resource "aws_route_table" "public_route_table" {
-    vpc_id = aws_vpc.main_vpc.id
-
-    route {
-        cidr_block: "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-    }
-
-    tags {
-        Name = "public_route_table"
-    }
-}
-
-resource "aws_route_table_association" "public_subnet_association" {
-    subnet_id = aws_subnet.public_subnet.id
-    route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table" "private_route_table" {
@@ -67,7 +40,12 @@ resource "aws_route_table" "private_route_table" {
     }
 }
 
-resource "aws_route_table_association" "private_subnet_association" {
-    subnet_id = aws_subnet.private_subnet.id
+resource "aws_route_table_association" "private_subnet_1_association" {
+    subnet_id = aws_subnet.private_subnet_1.id
+    route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+    subnet_id = aws_subnet.private_subnet_2.id
     route_table_id = aws_route_table.private_route_table.id
 }
