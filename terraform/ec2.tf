5b34bbf6dafd9d26776a433a3e80da543ff9b8f5
rupaut98 <rupakraut78@gmail.com>
2024-11-02 17:42:59 -0500

    ssh into EC2 works

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "taskapp" {
     ami = "ami-07c5ecd8498c59db5"
     instance_type = "t3.micro"
-    subnet_id = aws_subnet.private_subnet_1.id
-    associate_public_ip_address = false
+    subnet_id = aws_subnet.public_subnet.id
+    associate_public_ip_address = true
+    key_name = "temp"
 
     security_groups = [aws_security_group.ec2_sg.id]
 
@@ -21,6 +22,13 @@ resource "aws_security_group" "ec2_sg" {
         to_port     = 80
         protocol    = "tcp"
         cidr_blocks = ["10.0.0.0/16"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["69.246.48.44/32"]
     }
 
     egress {
--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -18,11 +18,42 @@ resource "aws_subnet" "private_subnet_2" {
     }
 }
 
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.main_vpc.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "us-west-2c"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public_subnet"
+    }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    tags = {
+        Name = "main_igw"
+    }
+}
+
 resource "aws_route_table" "private_route_table" {
     vpc_id = aws_vpc.main_vpc.id
 
     tags = {
         Name = "private_route_table"
+    }
+}
+
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main_igw.id
+    }
+    tags = {
+        Name = "public_route_table"
     }
 }
 
@@ -35,3 +66,9 @@ resource "aws_route_table_association" "private_subnet_2_association" {
     subnet_id = aws_subnet.private_subnet_2.id 
     route_table_id = aws_route_table.private_route_table.id 
 }
+
+resource "aws_route_table_association" "public_subnet_association" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_route_table.id
+} 
+  
