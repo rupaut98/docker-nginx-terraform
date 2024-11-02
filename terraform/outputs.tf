d43a5f55e02d50abe65869d04a14f337c383c790
rupaut98 <rupakraut78@gmail.com>
2024-11-02 02:30:13 -0500

    Added outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,22 @@ output "rds_endpoint" {
     description = "The endpoint of the RDS instance"
 }
 
+output "ec2_instance_id" {
+    value = aws_ec2_instance.taskapp.id
+    description = "The ID of the EC2 instance"
+}
+
+output "ec2_private_ip" {
+    value = aws_ec2_instance.taskapp.private_ip
+    description = "The private IP address of the EC2 instance"
+}
+
+output "ec2_cluster_name" {
+    value = aws_ecs_cluster.main_cluster.name
+    description = "The name of the ECS cluster"
+}
+
+output "ecs_service_arn" {
+    value = aws_ecs_service.ecs_service.arn
+    description = "The ARN of the ECS service"
+}
