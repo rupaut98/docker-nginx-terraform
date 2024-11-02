e121ce214aca99a404db33590e3dda0b3b5a567d
rupaut98 <rupakraut78@gmail.com>
2024-11-02 02:03:48 -0500

    Added subnet and sg for ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -4,10 +4,10 @@ resource "aws_ecs_cluster" "main_cluster" {
 
 resource "aws_ecs_task_definition" "task_definition" {
     family = "service"
-    network_mode = "vpc"
-    requires_compatibilities = ["FARGATE"]
+    network_mode = "bridge"
 
-    execution_role_arn = aws_iam_role_policy_attachment.ecs_task_execution_policy.policy_arn
+
+    execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
     container_definitions = jsonencode([
         {
             name = "first"
@@ -45,4 +45,18 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "ecs_service" {
+    name = "ecs-service"
+    cluster = aws_ecs_cluster.main_cluster.id
+    task_definition = aws_ecs_task_definition.task_definition.arn
+    launch_type = "EC2"
+
+    network_configuration {
+      subnets = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+      security_groups = [aws_security_group.rds_sg.id]
+    }
+
+    desired_count = 1
 }
